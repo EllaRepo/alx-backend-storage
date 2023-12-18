2c41b6af7d62165967b6a9bbaf00f8520b75f2d5
Eliab E <eliaberango@gmail.com>
2023-12-18 12:48:54 -0800

    Add 9-insert_school.py

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module defines a function that insert documents
+"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ insert into school
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id
