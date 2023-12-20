29920d34767938312baced823091f772d3f9c7f9
Eliab E <eliaberango@gmail.com>
2023-12-20 11:30:30 -0800

    Refactored

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,7 +4,19 @@
 import redis
 import requests
 from datetime import timedelta
+from functools import wraps
 
+
+def track_request_count(func):
+    """Decorator to track request count for a URL
+    """
+    @wraps(func)
+    def wrapper(url):
+        redis_store = redis.Redis()
+        req_key = f'count:{url}'
+        redis_store.incr(req_key)
+        return func(url)
+    return wrapper
 
 def get_page(url: str) -> str:
     """uses the requests module to obtain the HTML
@@ -12,13 +24,16 @@ def get_page(url: str) -> str:
     """
     if url is None or len(url.strip()) == 0:
         return ''
+
     redis_store = redis.Redis()
-    res_key = 'result:{}'.format(url)
-    req_key = 'count:{}'.format(url)
+    res_key = f'result:{url}'
     result = redis_store.get(res_key)
+
     if result is not None:
-        redis_store.incr(req_key)
-        return result
+        return result.decode('utf-8')
+
     result = requests.get(url).content.decode('utf-8')
+
     redis_store.setex(res_key, timedelta(seconds=10), result)
+
     return result
